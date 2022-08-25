58404fa75e785181fca6c76702f00e8c1e8f23cc
Gago55 <xgagik8@gmail.com>
2022-08-25 10:07:38 +0400

    Init project

---
+++ src/redux/appReducer.ts
@@ -0,0 +1,23 @@
+import { ThunkAction } from 'redux-thunk'
+import { InferActionsType, StateType } from './store'
+
+const initState = {
+}
+
+type InitStateType = typeof initState
+
+const appReducer = (state = initState, action: ActionsType): InitStateType => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export const actions = {
+    setTest: (value: number) => ({ type: '//SET_TEST', value } as const),
+}
+
+export type ActionsType = InferActionsType<typeof actions>
+type ThunkType = ThunkAction<Promise<void>, StateType, unknown, ActionsType>
+
+export default appReducer
